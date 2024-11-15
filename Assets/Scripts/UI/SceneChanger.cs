cd1cd676a514a8fcc49e500f092d4e0d3bb40896
keubung <skdud3390@naver.com>
2024-11-15 21:16:48 +0900

    [Add] TitleScene

---
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public SceneAsset scene;
+
+    public void ChangeScene()
+    {
+        if (scene != null)
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
