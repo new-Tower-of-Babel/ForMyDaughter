941c883dd34526e68ffff192e6f42985056b046e
Eunseol Park <kongpark2019@gmail.com>
2024-11-20 00:23:13 +0900

    [Complex]_Player_Park
    [Add]_Player
    플레이어컨트롤, 플레이어 애니메이션 추가
    
    [Chore]_PlayerSprites
    스프라이트 애니메이션을 위해 잘랐습니다

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool _isMoving = false;
+    private bool _isRunning = false;
+    private bool _isJumping = false;
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            
+            _isFacingRight = value;
+        }
+    }
+
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool IsJumping
+    {
+        get
+        {
+            return _isJumping;
+        }
+        set
+        {
+            _isJumping = value;
+            animator.SetBool("isJumping", value);
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnSit(InputAction.CallbackContext context)
+    {
+        
+    }
+    
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            //face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            //face the left
+            IsFacingRight = false;
+        }
+    }
+}
