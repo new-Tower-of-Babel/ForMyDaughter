2ae51f016f3de58e86d64b697aba463f9e05db19
keubung <skdud3390@naver.com>
2024-11-19 20:56:48 +0900

    [Fix] FadePanel & SceneChanger.cs

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -2,30 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     //public SceneAsset scene;
+    public Image fadeImage;
+    public float fadeDuration = 1.5f;
+
+    private static SceneChanger instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void StartButton()
     {
         //if (scene != null)
         //{
-        SceneManager.LoadScene("stage1");
+        //SceneManager.LoadScene("stage1");
+        LoadSceneWithFade("stage1");
         //}
     }
 
     public void LoadGame()
     {
         string savedStage = PlayerPrefs.GetString("SavedStage", "Stage1");
-        SceneManager.LoadScene(savedStage);
+        //SceneManager.LoadScene(savedStage);
+        LoadSceneWithFade(savedStage);
     }
 
     public void GoToTitle()
     {
         SaveGame();
-        SceneManager.LoadScene("TitleScene");
+        //SceneManager.LoadScene("TitleScene");
+        LoadSceneWithFade("TitleScene");
     }
 
     private void SaveGame()
@@ -44,7 +65,40 @@ public class SceneChanger : MonoBehaviour
         else if (currentScene == "Stage2") nextScene = "Stage3";
         else if (currentScene == "Stage3") nextScene = "TitleScene";
 
-        SceneManager.LoadScene(nextScene);
+        //SceneManager.LoadScene(nextScene);
+        LoadSceneWithFade(nextScene);
+    }
+    
+    public void LoadSceneWithFade(string scene)
+    {
+        StartCoroutine(FadeOutAndLoadScene(scene));
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        float elapsedTime = 0f;
+        Color color = fadeImage.color;
+
+        // 페이드 아웃 효과
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(0, 1, elapsedTime / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+
+        // 페이드 인 효과 (Optional)
+        elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1, 0, elapsedTime / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
     }
 
     public void Exit()
